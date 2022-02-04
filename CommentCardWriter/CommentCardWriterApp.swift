cd8185f29976574c63892c2e8ebe99b23d380f7b
Sean Okuyama-Smith <okuyama-smith.s@etoncollege.org.uk>
2022-02-04 12:04:42 +0000

    Initial Commit

---
+++ CommentCardWriter/CommentCardWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardWriterApp.swift
+//  CommentCardWriter
+//
+//  Created by Okuyama-Smith, Sean (NA) on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
